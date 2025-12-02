ee0df97e39db4172bf87104ae9456c9da99287fa
mishthimahajan <mishthimahajan0@gmail.com>
2025-12-03 01:47:51 +0530

    Update login API endpoint in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -400,7 +400,7 @@ const Login = () => {
     setError("");
     setLoading(true);
     try {
-      const response = await axios.post("https://foodlink-0jeg.onrender.com/request-food", {
+      const response = await axios.post("https://foodlink-0jeg.onrender.com/login", {
         email,
         password,
       });
